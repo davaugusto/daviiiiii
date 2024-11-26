18ae91542d1a47ff8b01b9477e5b436a3083c407
davaugusto <davicastro1449@gmail.com>
2024-11-25 21:18:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geral',
+    'Qual a capital da frança?',
+    'Paris'
+)
+
+criaCartao(
+    'Geral',
+    'Qual é o elemento representado o símbolo H?',
+    'Hidrogênio'
+)
+
+criaCartao(
+    'Geral',
+    'Quantops lado tem o triangulo?',
+    '3 lados'
+)
+
+criaCartao(
+    'Geral',
+    'Quem escreveu Dom Quixote?',
+    'Miguel de cervantes'
+)
